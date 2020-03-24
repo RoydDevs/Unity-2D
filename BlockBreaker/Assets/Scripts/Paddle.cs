d39aedde028a873b57f3747c7700577d50683db9
Mathieu <mathieu.sirere@gmail.com>
2020-03-24 17:54:16 +0100

    [BlockBreaker] Cours 53 : move paddle

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentPosX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        var paddlePos = new Vector2(currentPosX, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
