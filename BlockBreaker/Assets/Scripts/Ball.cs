e875e9e9b404761dc3a17e9148ede9354b3c3e17
Mathieu <mathieu.sirere@gmail.com>
2020-03-27 17:46:58 +0100

    [BlockBreaker] Cours 63

--- BlockBreaker/Assets/Scripts/Ball.cs
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -3,17 +3,22 @@ using Vector2 = UnityEngine.Vector2;
 
 public class Ball : MonoBehaviour
 {
+    //Config params
     [SerializeField] Paddle paddle1;
     [SerializeField] float xVelocity = 2f;
     [SerializeField] float yVelocity = 14f;
+    [SerializeField] AudioClip[] ballSounds;
 
+    //State
     private Vector2 paddleToBallVector;
     private bool hasStarted = false;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = this.transform.position - paddle1.transform.position;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -39,6 +44,15 @@ public class Ball : MonoBehaviour
     {
         var paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         this.transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(hasStarted)
+        {
+            var clip = ballSounds[Random.Range(0, ballSounds.Length - 1)];
+            audioSource.PlayOneShot(clip);
+        }
     }
 }
  
