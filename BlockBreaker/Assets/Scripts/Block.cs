670e6ea6323a7f58918d267fe302355fc6908d0c
Mathieu <mathieu.sirere@gmail.com>
2020-03-27 17:56:48 +0100

    [BlockBreaker] Cours 64

--- BlockBreaker/Assets/Scripts/Block.cs
+++ BlockBreaker/Assets/Scripts/Block.cs
@@ -2,8 +2,14 @@
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private AudioClip breakSound;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //Son plus fort : il part de la caméra
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        //Son plus faible -> il part du bloc
+        AudioSource.PlayClipAtPoint(breakSound, this.transform.position);
         Destroy(gameObject);
     }
 }
