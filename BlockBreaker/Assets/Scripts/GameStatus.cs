7a62fd446420bb1ea00ea0f3c2bd348b40c4566d
Mathieu <mathieu.sirere@gmail.com>
2020-03-27 18:45:20 +0100

    [BlockBreaker] Cours 67

---
+++ BlockBreaker/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [Range(0.1f, 15f)] [SerializeField] float gameSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
